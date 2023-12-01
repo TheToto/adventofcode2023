b7082c8fe2b65afa3dc414aef9fadfa032890ec3
Thomas Lupin <mail+git@thetoto.fr>
2023-12-02 00:27:32 +0100

    day1 part2

--- src/day1.ts
+++ src/day1.ts
@@ -4,8 +4,21 @@ const newLineCharCode = "\n".charCodeAt(0);
 const zeroCharCode = "0".charCodeAt(0);
 const nineCharCode = "9".charCodeAt(0);
 
-async function part1() {
-  let data = await fs.readFile("./input.txt");
+const numbers = [
+  "one",
+  "two",
+  "three",
+  "four",
+  "five",
+  "six",
+  "seven",
+  "eight",
+  "nine"
+];
+
+
+async function part1(data: Buffer) {
+
   let acc = 0;
   let first: number = -1;
   let last: number = -1;
@@ -24,8 +37,27 @@ async function part1() {
       last = ch - zeroCharCode;
     }
   }
+  if (first != -1) {
+    acc += first * 10 + last;
+  }
 
-  console.log("Part 1:", acc);
+  return acc;
 }
 
-part1();
+async function part2(data: Buffer) {
+  // Flemme d'optimiser
+  let fullText = data.toString('utf-8');
+
+  for (let i = 0; i < numbers.length; i++) {
+    fullText = fullText.replaceAll(numbers[i], numbers[i] + (i + 1).toString() + numbers[i]); // Yes, ugly.
+  }
+
+  return part1(Buffer.from(fullText));
+}
+
+fs.readFile("./input.txt").then(async (buffer) => {
+  let res1 = await part1(buffer);
+  console.log("Part 1:", res1);
+  let res2 = await part2(buffer);
+  console.log("Part 2:", res2);
+});
