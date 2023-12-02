72d29be4e4ed91b27678c5054ff7710a9521f6ab
Thomas Lupin <mail+git@thetoto.fr>
2023-12-02 12:31:40 +0100

    day2 opti

--- src/day2.ts
+++ src/day2.ts
@@ -2,26 +2,18 @@ import fs from "fs/promises";
 
 async function part1(data: Buffer) {
   let results = data.toString('utf-8').split("\n").map((line, i) => {
-    let [_, triesText] = line.split(":");
-    let tries = triesText.split(";");
-    for (let oneTry of tries) {
-      let picks = oneTry.split(",");
-      let blues = 0;
-      let reds = 0;
-      let greens = 0;
-      for (let pick of picks) {
-        pick = pick.trim();
-        let [count, color] = pick.split(" ");
-        color = color.trim();
-        if (color == "blue") {
-          blues += parseInt(count);
-        } else if (color == "red") {
-          reds += parseInt(count);
-        } else if (color == "green") {
-          greens += parseInt(count);
-        }
+    let [_, tries] = line.split(":");
+    for (let oneTry of tries.split(";")) {
+      let colors = {
+        blue: 0,
+        red: 0,
+        green: 0
+      };
+      for (let pick of oneTry.split(",")) {
+        let [count, color] = pick.trim().split(" ");
+        colors[color] += parseInt(count);
       }
-      if (reds > 12 || greens > 13 || blues > 14) {
+      if (colors.red > 12 || colors.green > 13 || colors.blue > 14) {
         return 0;
       }
     }
@@ -33,33 +25,27 @@ async function part1(data: Buffer) {
 
 async function part2(data: Buffer) {
   let results = data.toString('utf-8').split("\n").map((line, i) => {
-    let [_, triesText] = line.split(":");
-    let tries = triesText.split(";");
-    let minBlue = 0;
-    let minRed = 0;
-    let minGreen = 0;
-    for (let oneTry of tries) {
-      let picks = oneTry.split(",");
-      let blues = 0;
-      let reds = 0;
-      let greens = 0;
-      for (let pick of picks) {
-        pick = pick.trim();
-        let [count, color] = pick.split(" ");
-        color = color.trim();
-        if (color == "blue") {
-          blues += parseInt(count);
-        } else if (color == "red") {
-          reds += parseInt(count);
-        } else if (color == "green") {
-          greens += parseInt(count);
-        }
-      }
-      minBlue = Math.max(minBlue, blues);
-      minRed = Math.max(minRed, reds);
-      minGreen = Math.max(minGreen, greens);
+    let [_, tries] = line.split(":");
+    let mins = {
+      blue: 0,
+      red: 0,
+      green: 0
     }
-    return minBlue * minRed * minGreen
+    for (let oneTry of tries.split(";")) {
+      let colors = {
+        blue: 0,
+        red: 0,
+        green: 0
+      };
+      for (let pick of oneTry.split(",")) {
+        let [count, color] = pick.trim().split(" ");
+        colors[color] += parseInt(count);
+      }
+      mins.blue = Math.max(mins.blue, colors.blue);
+      mins.red = Math.max(mins.red, colors.red);
+      mins.green = Math.max(mins.green, colors.green);
+    }
+    return mins.blue * mins.red * mins.green;
   });
 
   return results.reduce((acc, val) => acc + val, 0);
